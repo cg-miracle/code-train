c3d80955ac99269cab901a56d0b03b97b95a6bc6
miracle <337544754@qq.com>
2020-01-30 21:36:48 +0800

    2020-01-30

--- Recursive/jumpFloor.js
+++ Recursive/jumpFloor.js
@@ -8,7 +8,7 @@ function jumpFloor(number) {
   let sum,
     step1 = 1,
     step2 = 1;
-  for (let i =2; i <= number; i++) {
+  for (let i = 2; i <= number; i++) {
     sum = step1 + step2;
     step1 = step2;
     step2 = sum;
@@ -20,16 +20,11 @@ function jumpFloor(number) {
 // 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
 // 求该青蛙跳上一个n级的台阶总共有多少种跳法。
 function jumpFloorII(number) {
-    if (number == 0) return 0;
-    if (number == 1) return 1;
-    let sum,
-      step1 = 1,
-      step2 = 1;
-    for (let i =2; i <= number; i++) {
-      sum = step1 + step2;
-      step1 = step2;
-      step2 = sum;
-    }
-    return sum;
+  if (number < 0) return 0;
+  let sum = 1;
+  for (let i = 1; i < number; i++) {
+    sum = 2 * sum;
   }
-console.log(jumpFloor(2))
+  return sum;
+}
+console.log(jumpFloorII(1));
